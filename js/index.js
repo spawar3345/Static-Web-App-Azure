dd77a214385ce326608e98c2d6456e86eb089f49
krishnamurtyp <krishnamurthy.pradhan@gmail.com>
2025-01-15 12:20:39 +0530

    updated message

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello Krishna Its your CI CD Pipeline!";
+  greeting.innerHTML = "Hello Krishna - we are showing CI CD Pipeline!";
 
   element.appendChild(greeting);
 
