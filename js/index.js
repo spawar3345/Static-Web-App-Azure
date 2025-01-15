6cbf97006c8ec7d6794f91d399b324080a71931b
spawar3345 <sanketpawar1200@gmail.com>
2025-01-15 12:40:20 +0530

    commit1

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello Krishna - we are seeing CI CD Pipeline!";
+  greeting.innerHTML = "Hello Sanket - we are seeing CI CD Pipeline!";
 
   element.appendChild(greeting);
 
